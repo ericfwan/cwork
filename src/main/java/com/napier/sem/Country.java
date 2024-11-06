2ac58a1f85465698830a82d838bce4bcf29cee4a
Eric <40683985@live.napier.ac.uk>
2024-11-06 13:11:45 +0000

    add world.sql

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,11 @@
+package com.napier.sem;
+
+public class Country {
+    public String name;
+    public int population;
+
+    // Constructor
+    public Country() {}
+
+    // Optionally, you can add getters and setters if required
+}
---
+++ src/main/java/com/napier/sem/PopulationReport.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+public class PopulationReport {
+    public String name;
+    public int totalPopulation;
+    public int cityPopulation;
+    public int nonCityPopulation;
+    public float cityPopulationPercentage;
+    public float nonCityPopulationPercentage;
+
+    // Constructor
+    public PopulationReport() {}
+
+    // Getters and setters (if needed)
+}
